70a29e13b7f296e5874b54b291e8a177df5628e2
Evgenii_Lobachev <lobachev9614@gmail.com>
2026-02-02 03:45:14 +0300

    add README.md

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ from http.server import HTTPServer
 
 from src.web_server import WebStore
 
-hostName = "localhost"
-serverPort = 8080
+hostName = "localhost" # Адрес для доступа по сети
+serverPort = 8080 # Порт для доступа по сети
 
 if __name__ == '__main__':
+
     web_server = HTTPServer((hostName, serverPort), WebStore)
     print("Сервер запущен http://%s:%s" % (hostName, serverPort))
 
